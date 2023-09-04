2b62032b5d850cdbc400073bbf8490698f65f7ef
vudinhphong <vudinhphong.261001@gmail.com>
2023-09-04 16:01:58 +0700

    Add configService and configModule, create module User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],  // Thêm ConfigModule vào đây
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({isGlobal:true}),// Cấu hình toàn cục, truy cập từ bất kỳ module nào trong ứng dụng mà không cần phải import lại ConfigModule
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+  
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  age: number;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  phone: number;
+
+  @Prop({ required: true , default: true})
+  isActive: boolean;
+
+  @Prop()
+  createAt: Date;
+
+  @Prop()
+  updateAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
