544b82d72dc74ff844fadc7ee409302429c79a77
vudinhphong <vudinhphong.261001@gmail.com>
2023-09-11 16:03:13 +0700

    Pagination part 1

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,8 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreateCompanyDto {
-    // @IsString({message: 'Phải là 1 chuỗi ký tự!'})
-    // @IsNotEmpty({message: 'Không được để trống!'})
-    // @IsEmail({},{message: 'Không đúng định dạng email!'})
     @IsNotEmpty({ message: 'Name được để trống!' })
     name: string
 
