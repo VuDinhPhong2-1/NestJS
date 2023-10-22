596c95692388eeb2b3e2abc7ae0902c6be4b2c08
vudinhphong <vudinhphong.261001@gmail.com>
2023-10-22 09:22:38 +0700

    Basic CRUD subscribers module

---
+++ src/subscribers/subscribers.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SubscribersService } from './subscribers.service';
+import { CreateSubscriberDto } from './dto/create-subscriber.dto';
+import { UpdateSubscriberDto } from './dto/update-subscriber.dto';
+import { ResponseMessage, User } from 'src/decorators/customize';
+import { IUser } from 'src/users/users.interface';
+
+@Controller('subscribers')
+export class SubscribersController {
+  constructor(private readonly subscribersService: SubscribersService) { }
+
+  @Post()
+  @ResponseMessage("Created a subs")
+  create(@Body() createSubscriberDto: CreateSubscriberDto, @User() user: IUser) {
+    return this.subscribersService.create(createSubscriberDto, user);
+  }
+
+  @Get()
+  findAll() {
+    return this.subscribersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.subscribersService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ResponseMessage("Updated a subs")
+  update(@Param('id') id: string, @Body() updateSubscriberDto: UpdateSubscriberDto,
+    @User() user: IUser) {
+    return this.subscribersService.update(id, updateSubscriberDto, user);
+  }
+
+  @Delete(':id')
+  @ResponseMessage("Delete a subs")
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.subscribersService.remove(id, user);
+  }
+}
